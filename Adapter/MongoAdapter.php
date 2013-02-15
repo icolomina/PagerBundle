aadfc4786db59fa0bb4f1efd1e38d39bc3c47e7e
nacho <nacho@localhost>
2013-02-15 17:30:46 +0000

    Creamos un adaptador para paginar colecciones Mongo a traves de un cursor MongoCursor

---
+++ Adapter/MongoAdapter.php
@@ -0,0 +1,72 @@
+<?php
+
+
+/*
+ * This file is part of the PagerBundle package.
+ *
+ * (c) Ignacio Colomina <ignacio.colomina@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MakerLabs\PagerBundle\Adapter;
+
+use MakerLabs\PagerBundle\Adapter\PagerAdapterInterface;
+
+class MongoAdapter implements PagerAdapterInterface \Iterator \Countable{
+   
+    protected $cursor;
+    
+    public function __construct(\MongoCursor $cursor){
+        
+        $this->cursor = $cursor;
+    }
+    
+    public function getTotalResults(){
+        
+        return $this->cursor->count();
+    }
+    
+    public function getResults($offset, $limit)
+    {
+        
+    }
+
+    public function isEmpty()
+    {
+        return $this->cursor->empty();
+    }
+    
+    public function current(){
+        
+        return $this->cursor->current();
+    }
+
+    public function key(){
+        
+        return $this->cursor->key();
+    }
+
+    public function next(){
+        
+        $this->cursor->next();
+    }
+
+    public function rewind(){
+        
+        $this->cursor->rewind();
+    }
+    
+    public function valid(){
+        
+        return $this->cursor->valid();
+    }
+
+    public function count(){
+        
+        return $this->getTotalResults();
+    }
+}
+
+?>
